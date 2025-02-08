72a99aa48e8997ae7a3c5f10e3974d439842800a
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-08 15:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const div = document.querySelector(".div")
+const cir1 = div.querySelectorAll(".circle")
+const prevBtn = document.querySelector("#prev")
+const nextBtn = document.querySelector("#next")
+prevBtn.disabled = true
+cir1[0].classList = "active"
+let i = 0;
+nextBtn.addEventListener("click",()=>{
+	i++
+	cir1[i].classList = "active"
+	prevBtn.disabled = false
+	if(i==4){
+		nextBtn.disabled = true
+	} 
+})
+prevBtn.addEventListener("click",()=>{
+	cir1[i].classList = "circle"
+	nextBtn.disabled = false
+	if(i==1){
+		prevBtn.disabled = true
+	}
+	i--
+})
+
+
+

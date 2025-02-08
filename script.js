061644c568524925f556bd9b840e9b9d9a9c25b3
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-08 16:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ prevBtn.disabled = true
 cir1[0].className = "active"
 let i = 0;
 nextBtn.addEventListener("click",()=>{
-	line[i].classList = "line"
+	line[i].className = "line"
 	i++
-	cir1[i].classList = "active"
+	cir1[i].className = "active"
 	prevBtn.disabled = false
 	if(i==4){
 		nextBtn.disabled = true
 	} 
 })
 prevBtn.addEventListener("click",()=>{
-	cir1[i].classList = "circle"
+	cir1[i].className = "circle"
 	nextBtn.disabled = false
 	if(i==1){
 		prevBtn.disabled = true
 	}
 	i--
-	line[i].classList = "line1"
+	line[i].className = "line1"
 })
 
 

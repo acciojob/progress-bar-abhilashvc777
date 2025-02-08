562238bc4c8f5693ad532a62add473c72a156dbf
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-08 16:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const div = document.querySelector(".div")
-const cir1 = document.getElementsByClassName(".circle")
+const cir1 = document.getElementsByClassName("circle")
 // const cir2 = div.querySelector("#circle-1")
 // const cir3 = div.querySelector("#circle-2")
 // const cir4 = div.querySelector("#circle-3")
@@ -8,7 +8,7 @@ const cir1 = document.getElementsByClassName(".circle")
 // const cir6 = div.querySelector("#circle-5")
 const prevBtn = document.querySelector("#prev")
 const nextBtn = document.querySelector("#next")
-const line = document.querySelectorAll(".line1")
+const line = document.getElementsByClassName("line1")
 prevBtn.disabled = true
 cir1[0].className = "active"
 let i = 0;
